80b88a6e31dd25ad91abf0681b9b4dd75abf7ce2
Mateusz Kostka <mateuszkostka5@gmail.com>
2023-12-27 20:35:20 +0100

    init commit

---
+++ FibonacciSeriesCalculator/FibbonaciCalculator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Numerics;
+
+namespace FibonacciSeriesCalculator;
+
+public static class FibbonaciCalculator
+{
+    public static IEnumerable<(ulong, BigInteger)> FibonacciIterative(ulong len)
+    {
+        BigInteger a = new(0);
+        BigInteger b = new(1);
+        BigInteger c = new(0);
+
+        yield return (0, a);
+        yield return (1, b);
+
+        for (ulong i = 2; i <= len; i++)
+        {
+            c = a + b;
+            yield return (i, c);
+            a = b;
+            b = c;
+        }
+    }
+}
